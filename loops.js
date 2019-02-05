e4c0177fdfaa02ed3bbd1290888342eb9d29cd69
Jordan Duran <jordanduranm@gmail.com>
2019-02-05 06:28:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,23 +1,28 @@
-function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+function forLoop(array){
+  for(var i = 0; i <25; i++){
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
   }
-  return array;
+}
+return array
 }
 
-function whileLoop(n) {
-  let countdown = n;
+var sample_arr = [];
+console.log(forLoop(sample_arr));
 
-    while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
-  }
 
+function whileLoop(n){
+  while(n>0)
+  {console.log(n--)}
+  return "done"
+}
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 function doWhileLoop(array){
 do {
   array.pop()
@@ -25,8 +30,5 @@ do {
 return array
 }
 
-  do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
-}
+do_while_arr = ['a','b','c'];
+console.log(doWhileLoop(do_while_arr))
