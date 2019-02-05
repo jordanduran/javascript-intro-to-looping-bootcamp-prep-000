e14d140ca4b4df7548234fc1febb0ea324bc1ef9
Jordan Duran <jordanduranm@gmail.com>
2019-02-05 07:09:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,36 @@
 function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
   }
-  return array;
+  return array
 }
 
 function whileLoop(n) {
-  let countdown = n;
-
-    while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
+  while (n > 0) {
+    console.log(--n);
   }
 
-function doWhileLoop(num) {
-  function incrementVariable() {
-  i = i + 1;
-  return console.log("I run once regardless.");
-}
+  return 'done'
 }
 
-do {
-  console.log("doo-bee-doo-bee-doo");
-} while (incrementVariable() < 5);
+
+
+function doWhileLoop(num) {
+
+  var i = 0;
+
+  function incrementVariable() {
+    i = i+1;
+    return i;
+  }
+
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+
+  return;
+}
